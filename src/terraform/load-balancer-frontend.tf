8232894e8cf6ddf0b0f01705451059d48337c268
markti <markti@outlook.com>
2023-10-22 13:11:59 -0400

    google_compute_http_health_check

--- src/terraform/load-balancer-frontend.tf
+++ src/terraform/load-balancer-frontend.tf
@@ -10,19 +10,13 @@ resource "google_compute_target_pool" "frontend" {
   instances = google_compute_instance.frontend.*.self_link
 
   health_checks = [
-    google_compute_health_check.frontend.name,
+    google_compute_http_health_check.frontend.name,
   ]
 }
 
-resource "google_compute_health_check" "frontend" {
-  name = "hc-${var.application_name}-${var.environment_name}-frontend"
-
-  timeout_sec         = 1
-  check_interval_sec  = 1
-  healthy_threshold   = 4
-  unhealthy_threshold = 5
-
-  tcp_health_check {
-    port = "5000"
-  }
+resource "google_compute_http_health_check" "frontend" {
+  name               = "${var.application_name}-${var.environment_name}-frontend"
+  request_path       = "/"
+  check_interval_sec = 1
+  timeout_sec        = 1
 }
