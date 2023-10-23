bd912e4941585d6a2285465bc970c58b6f598f07
markti <markti@outlook.com>
2023-10-22 22:53:33 -0400

    add global load balancer

--- src/terraform/load-balancer-frontend.tf
+++ src/terraform/load-balancer-frontend.tf
@@ -1,4 +1,32 @@
+resource "google_compute_backend_service" "backend_service" {
+  name        = "my-backend-service"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 10
 
+  backend {
+    group = google_compute_instance_group.instance_group.self_link
+  }
+
+  health_checks = [google_compute_http_health_check.frontend.self_link]
+}
+
+resource "google_compute_url_map" "url_map" {
+  name            = "my-url-map"
+  default_service = google_compute_backend_service.backend_service.self_link
+}
+
+resource "google_compute_target_http_proxy" "http_proxy" {
+  name    = "my-http-proxy"
+  url_map = google_compute_url_map.url_map.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "frontend" {
+  name        = "my-forwarding-rule"
+  ip_protocol = "TCP"
+  port_range  = "80"
+  target      = google_compute_target_http_proxy.http_proxy.self_link
+}
 
 /*
 resource "google_compute_forwarding_rule" "main" {
