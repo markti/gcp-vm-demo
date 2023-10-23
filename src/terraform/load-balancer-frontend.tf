2d3e7ad7df933b844664fc62cc5ba4786b9e9733
markti <markti@outlook.com>
2023-10-22 21:12:02 -0400

    add forwarding rule

--- src/terraform/load-balancer-frontend.tf
+++ src/terraform/load-balancer-frontend.tf
@@ -1,17 +1,24 @@
-/*
-resource "google_compute_forwarding_rule" "frontend" {
-  name   = "lb-${var.application_name}-${var.environment_name}-frontend"
-  region = var.primary_region
 
+resource "google_compute_forwarding_rule" "main" {
+  name                  = "${var.application_name}-${var.environment_name}"
+  target                = google_compute_target_pool.main.self_link
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = 80
+  region                = var.primary_region
+  ip_protocol           = "tcp"
 }
 
-resource "google_compute_target_pool" "frontend" {
-  name = "${var.application_name}-${var.environment_name}-frontend"
-
+resource "google_compute_target_pool" "main" {
+  name             = "${var.application_name}-${var.environment_name}"
+  region           = var.primary_region
+  session_affinity = "CLIENT_IP"
+  instances        = google_compute_instance.frontend.*.self_link
+  health_checks    = [google_compute_http_health_check.main.self_link]
 }
 
-resource "google_compute_http_health_check" "frontend" {
-  name = "${var.application_name}-${var.environment_name}-frontend"
+resource "google_compute_http_health_check" "main" {
+  name = "${var.application_name}-${var.environment_name}-hc"
 
+  port         = 5000
+  request_path = "/"
 }
-*/
