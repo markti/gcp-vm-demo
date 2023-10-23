b632786d3f0e9920e00723570d988ee7d02cb445
markti <markti@outlook.com>
2023-10-22 20:57:17 -0400

    remove all the things

--- src/terraform/load-balancer-frontend.tf
+++ src/terraform/load-balancer-frontend.tf
@@ -1,25 +1,17 @@
+/*
 resource "google_compute_forwarding_rule" "frontend" {
-  name                  = "lb-${var.application_name}-${var.environment_name}-frontend"
-  region                = var.primary_region
-  port_range            = 80
-  target                = google_compute_target_pool.frontend.self_link
-  load_balancing_scheme = "EXTERNAL"
+  name   = "lb-${var.application_name}-${var.environment_name}-frontend"
+  region = var.primary_region
+
 }
 
 resource "google_compute_target_pool" "frontend" {
   name = "${var.application_name}-${var.environment_name}-frontend"
 
-  instances = google_compute_instance.frontend.*.self_link
-
-  health_checks = [
-    google_compute_http_health_check.frontend.name,
-  ]
 }
 
 resource "google_compute_http_health_check" "frontend" {
-  name               = "${var.application_name}-${var.environment_name}-frontend"
-  request_path       = "/"
-  check_interval_sec = 5
-  timeout_sec        = 5
-  port               = 5000
+  name = "${var.application_name}-${var.environment_name}-frontend"
+
 }
+*/
--- src/terraform/security-group-frontend.tf
+++ src/terraform/security-group-frontend.tf
@@ -10,29 +10,3 @@ resource "google_compute_firewall" "allow_ssh" {
   source_ranges = ["0.0.0.0/0"] # Be cautious: This allows SSH from any IP. Adjust as necessary.
   target_tags   = ["ssh-access"]
 }
-
-resource "google_compute_firewall" "frontend_allow_http" {
-  name    = "allow-frontend-http"
-  network = google_compute_network.main.self_link
-
-  allow {
-    protocol = "tcp"
-    ports    = ["5000"]
-  }
-
-  source_ranges           = ["130.211.0.0/22", "35.191.0.0/16"]
-  target_service_accounts = [google_service_account.main.email]
-}
-
-resource "google_compute_firewall" "allow-external-to-lb" {
-  name    = "allow-external-to-lb"
-  network = google_compute_network.main.self_link
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
-
-  source_ranges           = ["35.191.0.0/16", "209.85.152.0/22", "209.85.204.0/22"]
-  target_service_accounts = [google_service_account.main.email]
-}
