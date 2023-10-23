3246e82992e4a5f27ed0f65aebaf1673844dd39b
markti <markti@outlook.com>
2023-10-22 21:02:57 -0400

    add firewalls

--- src/terraform/security-group-frontend.tf
+++ src/terraform/security-group-frontend.tf
@@ -10,3 +10,31 @@ resource "google_compute_firewall" "allow_ssh" {
   source_ranges = ["0.0.0.0/0"] # Be cautious: This allows SSH from any IP. Adjust as necessary.
   target_tags   = ["ssh-access"]
 }
+
+resource "google_compute_firewall" "default-lb-fw" {
+
+  name    = "${var.application_name}-${var.environment_name}-vm-service"
+  network = google_compute_network.main.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = [80]
+  }
+
+  source_ranges           = ["0.0.0.0/0"]
+  target_service_accounts = [google_service_account.main.email]
+}
+
+resource "google_compute_firewall" "default-hc-fw" {
+
+  name    = "${var.application_name}-${var.environment_name}-hc"
+  network = google_compute_network.main.self_link
+
+  allow {
+    protocol = "tcp"
+    ports    = [5000]
+  }
+
+  source_ranges           = ["35.191.0.0/16", "209.85.152.0/22", "209.85.204.0/22"]
+  target_service_accounts = [google_service_account.main.email]
+}
